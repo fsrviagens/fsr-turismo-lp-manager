99fc12f9fe2642a056051c03a2fe6e3b052be658
fsrviagens <contato@fsr.tur.br>
2025-10-09 12:09:53 -0300

    Criar pacotes_data.py

---
+++ pacotes_data.py
@@ -0,0 +1,12 @@
+# pacotes_data.py
+MOCK_PACOTES = [
+    {"id": 1, "nome": "Fortaleza com Praia da Lagoinha", "saida": "Curitiba, Londrina e Cascavel"},
+    {"id": 2, "nome": "Salvador com Morro de São Paulo", "saida": "Variadas"},
+    {"id": 3, "nome": "Egito 2X1 | Pacote 08 dias e 07 noites", "saida": "São Paulo, Rio de Janeiro (Bloqueios)"},
+    {"id": 4, "nome": "Réveillon na Tailândia com Festival das Lanternas", "saida": "Variadas"},
+    {"id": 5, "nome": "Lençóis Maranhenses - São Luís e Barreirinhas", "saida": "Variadas"},
+    {"id": 6, "nome": "Porto de Galinhas e Maragogi", "saida": "Londrina e Maringá"},
+    {"id": 7, "nome": "Réveillon 2026 - Costão do Santinho Resort", "saida": "Florianópolis"},
+    {"id": 8, "nome": "Grand Turquia Extra 2026 Essencial", "saida": "Variadas"},
+    {"id": 9, "nome": "Montevidéu Bloqueio Exclusivo", "saida": "Variadas"},
+]
