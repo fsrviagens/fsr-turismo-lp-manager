21d26851c8fd00a4684de4f7766fbd8450d5c6a7
fsrviagens <contato@fsr.tur.br>
2025-10-17 17:20:17 -0300

    Atualizar o urls.py

--- agencia_app/urls.py
+++ agencia_app/urls.py
@@ -7,11 +7,10 @@ urlpatterns = [
     # URL principal da Landing Page (por exemplo: http://fsr.tur.br/)
     path('', views.landing_page, name='landing_page'),
     
-    # URL para onde o formulário será submetido (por exemplo: http://fsr.tur.br/capturar/)
+    # URL para onde o formulário será submetido (POST)
     path('capturar/', views.capturar_lead, name='capturar_lead'),
 ]
 
 # Documentação:
-# - path('', ...): Define o caminho da URL. O vazio '' significa a raiz do site.
-# - name='...': Dá um nome para a URL, o que permite usarmos 'reverse('nome')' em outras partes do código.
-# - views.landing_page: Liga esta URL à função 'landing_page' dentro de views.py.
+# - path('', ...): Liga a raiz do aplicativo (app) à view que exibe o CMS.
+# - path('capturar/', ...): Liga o caminho de submissão do formulário à view que salva o Lead.
