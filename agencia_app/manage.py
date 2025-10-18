53960c40a59a4b71b9e6fabd0cc8df4d36c655af
Fabio Souza Rodrigues <contato@fsr.tur.br>
2025-10-18 08:52:37 -0300

    Atualizar o manage.py

--- agencia_app/manage.py
+++ agencia_app/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    # AQUI ESTÁ A CORREÇÃO: Apontando para o seu diretório de settings
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agencia_app.settings') 
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
